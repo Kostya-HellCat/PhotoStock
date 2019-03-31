629e0f8e06a04eb6a0ce203bdb51ac0f5eee2305
Kostya HellCat <kostik.atroshenkov@mail.ru>
2019-03-31 21:28:41 +0300

    Snack bar

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -26,84 +26,83 @@ class AuthRoute extends StatelessWidget {
       ),
       //backgroundColor: Colors.indigo[400],
       resizeToAvoidBottomPadding: false,
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Form(
-            key: _formKey,
-            child: Padding(
-              padding: EdgeInsets.only(left: 50,right: 50,top: 50),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      //hintText: 'Логин или E-Mail',
-                      labelText: 'Логин',
+      body: Builder(
+        builder: (context) => Center(
+          child: Column(
+            children: <Widget>[
+              Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.only(left: 50,right: 50,top: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        //hintText: 'Логин или E-Mail',
+                        labelText: 'Логин',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Пожалуйста, введите логин';
+                        }
+                      },
                     ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Пожалуйста, введите логин';
-                      }
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.lock),
-                      //hintText: 'Логин или E-Mail',
-                      labelText: 'Пароль',
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.lock),
+                        //hintText: 'Логин или E-Mail',
+                        labelText: 'Пароль',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Пожалуйста, введите пароль';
+                        }
+                      },
                     ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Пожалуйста, введите пароль';
-                      }
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Center(
-                      child: RaisedButton(
-                        onPressed: () {
-
-                          if (_formKey.currentState.validate()) {
-                            // If the form is valid, we want to show a Snackbar
-                            Scaffold.of(context)
-                                .showSnackBar(SnackBar(content: Text('Обработка...')));
-                            http.get('https://json.flutter.su/echo').then((response) {
-                              print("Response status: ${response.statusCode}");
-                              print("Response body: ${response.body}");
-                            }).catchError((error){
-                              print("Error: $error");
-
-                            });
-                          }
-                        },
-                        child: Text('Войти'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Center(
+                        child: RaisedButton(
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              // If the form is valid, we want to show a Snackbar
+                              Scaffold.of(context).showSnackBar(SnackBar(content: Text('Обработка...')));
+                              http.get('https://json.flutter.su/echo').then((response) {
+                                print("Response status: ${response.statusCode}");
+                                print("Response body: ${response.body}");
+                              }).catchError((error){
+                                print("Error: $error");
+                              });
+                            }
+                          },
+                          child: Text('Войти'),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            )
-        ),
-           FlatButton(
-            child: Text('Забыли пароль?'),
-            onPressed: () {
-              /* Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => RegRoute()),
-              ); */
+                  ],
+                ),
+              )
+          ),
+             FlatButton(
+              child: Text('Забыли пароль?'),
+              onPressed: () {
+                /* Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegRoute()),
+                ); */
+              },
+            ),
+            FlatButton(
+              child: Text('Регистрация'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/reg');
             },
           ),
-          FlatButton(
-            child: Text('Регистрация'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/reg');
-          },
-        ),
-        ],
+          ],
+          ),
         ),
       ),
     );
@@ -113,86 +112,87 @@ class AuthRoute extends StatelessWidget {
 class RegRoute extends StatelessWidget {
   final _formKey2 = GlobalKey<FormState>();
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context2) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Регистрация"),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Form(
-                key: _formKey2,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 50,right: 50,top: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.person),
-                          labelText: 'Логин',
+      body: Builder(
+        builder: (context2) => Center(
+          child: Column(
+            children: <Widget>[
+              Form(
+                  key: _formKey2,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 50,right: 50,top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.person),
+                            labelText: 'Логин',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Пожалуйста, введите логин';
+                            }
+                          },
                         ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Пожалуйста, введите логин';
-                          }
-                        },
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.alternate_email),
-                          labelText: 'E-mail',
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.alternate_email),
+                            labelText: 'E-mail',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Пожалуйста, введите E-mail';
+                            }
+                          },
                         ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Пожалуйста, введите E-mail';
-                          }
-                        },
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.lock),
-                          labelText: 'Пароль',
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.lock),
+                            labelText: 'Пароль',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Пожалуйста, введите пароль';
+                            }
+                          },
                         ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Пожалуйста, введите пароль';
-                          }
-                        },
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.local_phone ),
-                          labelText: 'Телефон',
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.local_phone ),
+                            labelText: 'Телефон',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Пожалуйста, введите номер телефона';
+                            }
+                          },
                         ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Пожалуйста, введите номер телефона';
-                          }
-                        },
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: Center(
-                          child: RaisedButton(
-                            onPressed: () {
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: Center(
+                            child: RaisedButton(
+                              onPressed: () {
 
-                              if (_formKey2.currentState.validate()) {
-                                // If the form is valid, we want to show a Snackbar
-                                Scaffold.of(context)
-                                    .showSnackBar(SnackBar(content: Text('Обработка...')));
-                              }
-                            },
-                            child: Text('Зарегистрироваться'),
+                                if (_formKey2.currentState.validate()) {
+                                  // If the form is valid, we want to show a Snackbar
+                                  Scaffold.of(context2).showSnackBar(SnackBar(content: Text('Обработка...')));
+                                }
+                              },
+                              child: Text('Зарегистрироваться'),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                )
-            ),
-          ],
+                      ],
+                    ),
+                  )
+              ),
+            ],
+          ),
         ),
       ),
     );
